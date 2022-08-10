910b135b55fd163d5a45eeaafc69d3b3bfd9ee43
eniojb <eniojb@gmail.com>
2022-08-10 13:21:34 +0000

    Adicionado codigo HTML ao retorno da função de contatos

--- app.py
+++ app.py
@@ -6,7 +6,16 @@ app = Flask("Hello")
 def hello():
     return "Hello World"
 
-
-    @app.route("/contatos")
-    def contato():
-        return "Enio J.Barboza, e-mail"
+@app.route("/contatos")
+def contato():
+        return """<html>
+            <head>
+                <Contatos>
+            </head>
+            <body>
+                <h1>Enio J.Barboza</h1>
+                <h2>enio.email@email.com</h2>
+                <h3>Contato: 119999-9999</h3>
+            </body>
+        </html>"""
+    
