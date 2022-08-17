825500e7d4bf48ca3d761a78a8093116a92a2b98
eniojb <eniojb@gmail.com>
2022-08-17 14:57:22 +0000

    Adicionado os css

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask("Hello")
 
+posts = [
+    {
+        "title": "Meu primeiro posts",
+        "body": "Esse post é o primeiro do blog",
+        "autor": "Danilo Souza Miguel",
+        "created": datetime(2022,8,17)
+
+    },
+    {
+        "title": "Meu segundo posts",
+        "body": "Esse post é o primeiro do blog",
+        "autor": "Danilo Souza Miguel",
+        "created": datetime(2022,8,17)
+
+    },
+    {
+        "title": "Meu terceiro posts",
+        "body": "Esse post é o primeiro do blog",
+        "autor": "Danilo Souza Miguel",
+        "created": datetime(2022,8,17)
+
+    }        
+]
+
+
 @app.route("/")
-def hello():
-    return "Hello World"
-
-@app.route("/usuario")
-def usuario():
-    usuario = [1, "Danilo de Souza Miguel", "Professor"]
-    alunos =["Andre Guedes","Lucas Santos","Alicia Duarte","Raiane Carpline"] 
-    return render_template("index.html", usuario = usuario, alunos = alunos)
-
-@app.route("/contatos")
-def contato():
-    nomeAula = "Aula python para Web"   
-    return render_template("index.html", nome = nomeAula)
-
-
+def index():
+    return render_template("index.html", posts=posts)
